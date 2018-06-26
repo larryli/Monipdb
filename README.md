51102091ce6c9717495b5c502b3a920cf1630b69
Larry Li <larryli@qq.com>
2018-06-26 19:09:24 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Monipdb
 使用 PHP 数组式访问 Monipdb 数据库
+
+## 使用方法
+
+```php
+$monipdb = new \larryli\monipdb\Monipdb('17monipdb.datx');
+
+// find
+echo "202.103.24.68: {$monipdb['202.103.24.68']}\n";
+
+// dump
+for foreach ($monipdb as $ip => $name) {
+    echo "{$ip}: $name\n";
+}
+```
--- example/monipdb.php
+++ example/monipdb.php
@@ -1,51 +1,51 @@
 <?php
 
-require('../src/Monipdb.php');
+require(dirname(__DIR__) . '/src/Monipdb.php');
 
 echo "[MEMORY] Start: " . memory_get_usage() . "\n";
 
 try {
-    $dat = new \larryli\monipdb\Monipdb('17monipdb.datx');
+    $monipdb = new \larryli\monipdb\Monipdb(dirname(__DIR__) . '/17monipdb.datx');
 } catch (Exception $e) {
     die($e);
 }
 
 echo "[MEMORY] DatX: " . memory_get_usage() . "\n";
 
-echo "Count: " . count($dat) . "\n";
+echo "Count: " . count($monipdb) . "\n";
 
-foreach ($dat as $ip => $name) {
+foreach ($monipdb as $ip => $name) {
     echo "{$ip}: $name\n";
     break;
 }
 
-echo "202.103.24.68: {$dat['202.103.24.68']}\n";
+echo "202.103.24.68: {$monipdb['202.103.24.68']}\n";
 
 $ip = ip2long('202.96.134.133');
-echo "3395323525: {$dat[3395323525]}\n";
+echo "3395323525: {$monipdb[3395323525]}\n";
 
-unset($dat);
+unset($monipdb);
 
 echo "[MEMORY] Clean: " . memory_get_usage() . "\n";
 
 try {
-    $dat = new \larryli\monipdb\Monipdb('17monipdb.dat', false);
+    $monipdb = new \larryli\monipdb\Monipdb(dirname(__DIR__) . '/17monipdb.dat', false);
 } catch (Exception $e) {
     die($e);
 }
 
 echo "[MEMORY] Dat: " . memory_get_usage() . "\n";
 
-echo "Count: " . count($dat) . "\n";
+echo "Count: " . count($monipdb) . "\n";
 
-foreach ($dat as $ip => $name) {
+foreach ($monipdb as $ip => $name) {
     echo "{$ip}: $name\n";
     break;
 }
 
-echo "202.103.24.68: {$dat['202.103.24.68']}\n";
+echo "202.103.24.68: {$monipdb['202.103.24.68']}\n";
 
-echo "3395323525: {$dat[3395323525]}\n";
+echo "3395323525: {$monipdb[3395323525]}\n";
 
-unset($dat);
+unset($monipdb);
 echo "[MEMORY] Clean: " . memory_get_usage() . "\n";
