f55b622d40de18375b1ac1bb7ceba3393e244914
Larry Li <larryli@qq.com>
2018-06-27 16:06:56 +0800

    benchmark

--- example/monipdb.php
+++ example/monipdb.php
@@ -2,6 +2,18 @@
 
 require(dirname(__DIR__) . '/vendor/autoload.php');
 
+function info($title, $start = false)
+{
+    static $keep;
+
+    echo "[MEMORY] {$title}: " . memory_get_usage() . "\n";
+    if ($start) {
+        $keep = microtime(true);
+    } else {
+        echo "[TIME] {$title}: " . round(microtime(true) - $keep, 3) . "\n";
+    }
+}
+
 $objects = array(
     'Monipdb' => '\larryli\monipdb\Monipdb',
     'CachedMonipdb' => '\larryli\monipdb\CachedMonipdb',
@@ -14,13 +26,26 @@ $files = array(
 
 foreach ($objects as $object => $class) {
     foreach ($files as $name => $params) {
-        echo "---- {$object} {$name} ----\n\n";
+        echo "\n---- {$object} {$name} ----\n\n";
 
-        echo "[MEMORY] Start: " . memory_get_usage() . "\n";
+        info('Start', true);
 
         try {
             $monipdb = new $class($params[0], $params[1]);
-            echo "[MEMORY]  Load: " . memory_get_usage() . "\n";
+            info('Load');
+
+            echo "[GET] 202.103.24.68: {$monipdb['202.103.24.68']}\n";
+
+            $ip = ip2long('202.96.134.133');
+            echo "[GET] 3395323525: {$monipdb[3395323525]}\n";
+
+            $benchmark = array();
+            for ($n = 0; $n < 1000; $n++) {
+                $benchmark[] = $monipdb['202.103.24.68'];
+            }
+            echo "[BENCHMARK] 202.103.24.68: {$benchmark[0]}\n";
+            unset($benchmark);
+            info('Benchmark');
 
             echo "[COUNT] Count: " . count($monipdb) . "\n";
 
@@ -32,18 +57,13 @@ foreach ($objects as $object => $class) {
                 $n++;
             }
 
-            echo "[GET] 202.103.24.68: {$monipdb['202.103.24.68']}\n";
-
-            $ip = ip2long('202.96.134.133');
-            echo "[GET] 3395323525: {$monipdb[3395323525]}\n";
-
-            echo "[MEMORY]   End: " . memory_get_usage() . "\n";
+            info('End');
             unset($monipdb);
         } catch (Exception $e) {
             die($e);
         }
 
         gc_collect_cycles();
-        echo "[MEMORY] Clean: " . memory_get_usage() . "\n\n";
+        info('Clean');
     }
 }
