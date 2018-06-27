26bbc5c6fd424520bcdf5b15ce8bffee2e436e40
Larry Li <larryli@qq.com>
2018-06-27 13:02:59 +0800

    opt example

--- example/monipdb.php
+++ example/monipdb.php
@@ -2,51 +2,42 @@
 
 require(dirname(__DIR__) . '/src/Monipdb.php');
 
-echo "[MEMORY] Start: " . memory_get_usage() . "\n";
+$objects = array(
+    'Monipdb' => '\larryli\monipdb\Monipdb',
+);
+$files = array(
+    'DatX' => array(dirname(__DIR__) . '/17monipdb.datx', true),
+    'Dat' => array(dirname(__DIR__) . '/17monipdb.dat', false)
+);
 
-try {
-    $monipdb = new \larryli\monipdb\Monipdb(dirname(__DIR__) . '/17monipdb.datx');
-} catch (Exception $e) {
-    die($e);
+foreach ($objects as $object => $class) {
+    foreach ($files as $name => $params) {
+        echo "---- {$object} {$name} ----\n\n";
+
+        echo "[MEMORY] Start: " . memory_get_usage() . "\n";
+
+        try {
+            $monipdb = new $class($params[0], $params[1]);
+            echo "[MEMORY] Load: " . memory_get_usage() . "\n";
+
+            echo "[COUNT] Count: " . count($monipdb) . "\n";
+
+            foreach ($monipdb as $ip => $name) {
+                echo "[FOREACH] {$ip}: $name\n";
+                break;
+            }
+
+            echo "[GET] 202.103.24.68: {$monipdb['202.103.24.68']}\n";
+
+            $ip = ip2long('202.96.134.133');
+            echo "[GET] 3395323525: {$monipdb[3395323525]}\n";
+
+            unset($monipdb);
+        } catch (Exception $e) {
+            die($e);
+        }
+
+        gc_collect_cycles();
+        echo "[MEMORY] Clean: " . memory_get_usage() . "\n\n";
+    }
 }
-
-echo "[MEMORY] DatX: " . memory_get_usage() . "\n";
-
-echo "Count: " . count($monipdb) . "\n";
-
-foreach ($monipdb as $ip => $name) {
-    echo "{$ip}: $name\n";
-    break;
-}
-
-echo "202.103.24.68: {$monipdb['202.103.24.68']}\n";
-
-$ip = ip2long('202.96.134.133');
-echo "3395323525: {$monipdb[3395323525]}\n";
-
-unset($monipdb);
-gc_collect_cycles();
-echo "[MEMORY] Clean: " . memory_get_usage() . "\n";
-
-try {
-    $monipdb = new \larryli\monipdb\Monipdb(dirname(__DIR__) . '/17monipdb.dat', false);
-} catch (Exception $e) {
-    die($e);
-}
-
-echo "[MEMORY] Dat: " . memory_get_usage() . "\n";
-
-echo "Count: " . count($monipdb) . "\n";
-
-foreach ($monipdb as $ip => $name) {
-    echo "{$ip}: $name\n";
-    break;
-}
-
-echo "202.103.24.68: {$monipdb['202.103.24.68']}\n";
-
-echo "3395323525: {$monipdb[3395323525]}\n";
-
-unset($monipdb);
-gc_collect_cycles();
-echo "[MEMORY] Clean: " . memory_get_usage() . "\n";
